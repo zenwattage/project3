d098f9d2e002882a5662064333596538b9e184fd
zenwattage <zenwattage@gmail.com>
2019-07-16 01:23:24 -0700

    initial express strucuture, routes created, initial components created, react server created, all linked

---
+++ project3MernApp/app.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const PORT = process.env.PORT || 8080;
+const log = console.log;
+
+const app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.listen(PORT, () => {
+  log('Server listening on PORT ${PORT}');
+});
+
+
+module.exports = app;
