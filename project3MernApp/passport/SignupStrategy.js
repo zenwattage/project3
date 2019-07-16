01dd086dfde6e284364b564fe56b13c160c38737
zenwattage <zenwattage@gmail.com>
2019-07-16 02:25:19 -0700

    adding done callback to signupstrategy.js

--- project3MernApp/passport/SignupStrategy.js
+++ project3MernApp/passport/SignupStrategy.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 const SignupStrategy = new Strategy(function(username, password, done) {
   const user = username;
   //what should be happening once user is signing up
+  done('Oops, something happened');
 });
 
 module.exports = SignupStrategy;
