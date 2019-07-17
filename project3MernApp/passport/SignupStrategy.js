f200ec4e8e864e0f1192bc8dbd470bc8081354da
Scott H <zenwattage@gmail.com>
2019-07-17 10:41:25 -0700

    adding bcrypt for password encryption

--- project3MernApp/passport/SignupStrategy.js
+++ project3MernApp/passport/SignupStrategy.js
@@ -1,10 +1,37 @@
 const Strategy = require('passport-local').Strategy;
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 
-const SignupStrategy = new Strategy(function(username, password, done) {
-  const user = username;
-  //what should be happening once user is signing up
-  done(null, 'User signed up');
+const SignupStrategy = new Strategy({ passReqToCallback: true }, function (req, username, password, done) {
+  //get users email
+  const email = req.body.email;
+
+  User.findOne({
+    email
+  }).lean().exec((err, user) => {
+    if (err) {
+      return done(err, null);
+    }
+    if (user) {
+      return cb('User already exists', null);
+    }
+
+    //get password and encrypt with bcrypt
+    const encryptedPassword = bcrypt.hashSync(password, salt);
+    let newUser = new User({
+      email,
+      password: encryptedPassword
+    });
+
+    newUser.save((error, inserted) => {
+      if (error) {
+        return done(error, null);
+      }
+
+      return done(null, inserted);
+    });
+  });
 });
 
 module.exports = SignupStrategy;
