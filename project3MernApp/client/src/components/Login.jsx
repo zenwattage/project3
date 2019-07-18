75372d8c26dd0413397c7ed9e7690b68ac8ecea9
Scott H <zenwattage@gmail.com>
2019-07-18 12:31:31 -0700

    handsubmit - isauthenticated added

--- project3MernApp/client/src/components/Login.jsx
+++ project3MernApp/client/src/components/Login.jsx
@@ -1,42 +1,56 @@
-import React, { Component } from 'react';
-import { } from 'react-router-dom'
-import axios from 'axios';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import axios from "axios";
 
 export default class Login extends Component {
-
-  state = {   
-  };
-
-  componentDidMount = () => {
-    const isAuthenticated = window.localStorage.getItem('isAuthenticated');
-
-    if(isAuthenticated) {
-
-    }
-  };
+  state = {};
 
   handleSubmit = event => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    axios({
+      url: "/authentication/signin",
+      method: "POST",
+      data: {
+        email,
+        password
+      }
+    })
+    .then(response => {
+      const isAuthenticated = response.data.isAuthenticated;
+      window.localStorage.setItem('isAuthenticated', isAuthenticated);
+      this.props.history.push("/profile");
+    })
+    .catch(error => {
+      this.setState({
+        errorMessage: error.response.data.message
+      });
+    });
   };
 
-  handleChange = (event) => {
-    const {name, value} = event.target;
+  handleChange = event => {
+    const { name, value } = event.target;
     this.setState({
       [name]: value
-    })
+    });
   };
-    render() {
-      // JSX
-      return (
-          <div>
-            <h1> Login Component </h1>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" name="email" onChange={this.handleChange} />
-              <input type="password" name="password" onChange={this.handleChange} />
+  render() {
+    const isAuthenticated = window.localStorage.getItem("isAuthenticated");
 
-              <button>Login</button>
-            </form>
-            <p>{this.state.errorMessage}</p>
-          </div>
-      );
+    if (isAuthenticated) {
+      return <Redirect to="/profile" />;
     }
+    // JSX
+    return (
+      <div>
+        <h1> Login Component </h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="email" onChange={this.handleChange} />
+          <input type="password" name="password" onChange={this.handleChange} />
+          <button>Login</button>
+        </form>
+        <p>{this.state.errorMessage}</p>
+      </div>
+    );
+  }
 }
